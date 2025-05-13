5c83655c1d0235e58b44f44aa4a15a01b7c2305c
silvaglez <gazaping@hotmail.com>
2025-05-13 16:29:20 +0200

    Refactoring

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import content from '@astrojs/content';
-
-export default defineConfig({
-  site: 'https://atlantyde-labs.vercel.app',
-  integrations: [mdx(), sitemap(), content()],
-});
